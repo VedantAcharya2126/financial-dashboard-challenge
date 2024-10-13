e6d720cddebb667eff86288c1dbbc6294ef1d6bc
Vedant <vedantacharya2000@gmail.com>
2024-10-13 07:55:24 -0400

    Intializing first spring boot project

---
+++ src/main/java/com/challenge/intelliware/financial_dashboard_challenge/FinancialDashboardChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.intelliware.financial_dashboard_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialDashboardChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialDashboardChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/intelliware/financial_dashboard_challenge/FinancialDashboardChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.intelliware.financial_dashboard_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialDashboardChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
